266bc7931d13a8d720c9ea75c8c7beeb6b481b23
José Matías Misiego Ruiz <matias.misiego@gmail.com>
2020-07-24 14:37:10 +0200

    left/right/up/down movements and falling after some elapsed time

--- src/main.rs
+++ src/main.rs
@@ -1,90 +1,250 @@
-use ggez::*;
-use ggez::graphics::{BLACK, Text, DrawParam, TextFragment, Mesh, DrawMode, Rect, Color, WHITE};
+use ggez::graphics::{BLACK};
 use ggez::event::{KeyMods, KeyCode};
-use ggez::nalgebra as na;
+use ggez::{nalgebra as na, GameResult, Context, ContextBuilder, conf, graphics, event, timer};
+use std::env;
+use std::path;
 
-type Point2 = na::Point2<f32>;
+type ScreenPoint2 = na::Point2<f32>;
+type WorldPoint2 = na::Point2<i8>;
+type WorldVector2 = na::Vector2<i8>;
 
 #[derive(Debug)]
-enum ActorType {
-    Block,
+enum TetriminoType {
+    I,
+    J,
+    L,
+    O,
+    S,
+    T,
+    Z,
 }
 
-#[derive(Debug)]
-struct Actor {
-    tag: ActorType,
-    pos: Point2,
+struct Assets {
+    block_image: graphics::Image,
 }
 
-fn create_block() -> Actor {
-    Actor {
-        tag: ActorType::Block,
-        pos: Point2::origin(),
+impl Assets {
+    fn new(ctx: &mut Context) -> GameResult<Assets> {
+        let block_image = graphics::Image::new(ctx, "/block.png")?;
+
+        Ok(Assets {
+            block_image,
+        })
+    }
+
+    fn block_image(&mut self, tetrimino: &Tetrimino) -> &mut graphics::Image {
+        match tetrimino.kind {
+            TetriminoType::I => &mut self.block_image,
+            TetriminoType::J => &mut self.block_image,
+            TetriminoType::L => &mut self.block_image,
+            TetriminoType::O => &mut self.block_image,
+            TetriminoType::S => &mut self.block_image,
+            TetriminoType::T => &mut self.block_image,
+            TetriminoType::Z => &mut self.block_image,
+        }
     }
 }
 
-struct State {
-    block: Actor,
-    dt: std::time::Duration,
-    pos_x: f32,
+#[derive(Debug)]
+struct Tetrimino {
+    kind: TetriminoType,
+    pos: WorldPoint2,
+    vectors: [WorldVector2; 3],
 }
 
-impl ggez::event::EventHandler for State {
+impl Tetrimino {
+    const COUNTER_CLOCKWISE_MATRIX: [[i8; 2]; 2] = [[0, -1], [1, 0]];
+    const CLOCKWISE_MATRIX: [[i8; 2]; 2] = [[0, 1], [-1, 0]];
+
+    fn from(kind: TetriminoType) -> Tetrimino {
+        match kind {
+            TetriminoType::I => Tetrimino {
+                kind: TetriminoType::I,
+                pos: WorldPoint2::new(4, 0),
+                vectors: [WorldVector2::new(-1, 0), WorldVector2::new(1, 0), WorldVector2::new(2, 0)],
+            },
+            TetriminoType::J => Tetrimino {
+                kind: TetriminoType::J,
+                pos: WorldPoint2::origin(),
+                vectors: [WorldVector2::new(-2, 0), WorldVector2::new(-1, 0), WorldVector2::new(0, -1)],
+            },
+            TetriminoType::L => Tetrimino {
+                kind: TetriminoType::L,
+                pos: WorldPoint2::origin(),
+                vectors: [WorldVector2::new(-2, 0), WorldVector2::new(-1, 0), WorldVector2::new(0, -1)],
+            },
+            TetriminoType::O => Tetrimino {
+                kind: TetriminoType::O,
+                pos: WorldPoint2::origin(),
+                vectors: [WorldVector2::new(-2, 0), WorldVector2::new(-1, 0), WorldVector2::new(0, -1)],
+            },
+            TetriminoType::S => Tetrimino {
+                kind: TetriminoType::S,
+                pos: WorldPoint2::origin(),
+                vectors: [WorldVector2::new(-2, 0), WorldVector2::new(-1, 0), WorldVector2::new(0, -1)],
+            },
+            TetriminoType::T => Tetrimino {
+                kind: TetriminoType::T,
+                pos: WorldPoint2::origin(),
+                vectors: [WorldVector2::new(-2, 0), WorldVector2::new(-1, 0), WorldVector2::new(0, -1)],
+            },
+            TetriminoType::Z => Tetrimino {
+                kind: TetriminoType::Z,
+                pos: WorldPoint2::origin(),
+                vectors: [WorldVector2::new(-2, 0), WorldVector2::new(-1, 0), WorldVector2::new(0, -1)],
+            },
+        }
+    }
+
+    fn move_left(&mut self) {
+        self.pos.x += -1;
+    }
+
+    fn move_right(&mut self) {
+        self.pos.x += 1;
+    }
+
+    fn move_down(&mut self) {
+        self.pos.y += 1;
+    }
+
+    fn move_up(&mut self) {
+        self.pos.y += -1;
+    }
+}
+
+const FALL_TIME: f32 = 5.0;
+
+struct MainState {
+    assets: Assets,
+    screen_width: f32,
+    screen_height: f32,
+    tetrimino: Tetrimino,
+    fall_timeout: f32,
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        println!("Game resource path: {:?}", ctx.filesystem);
+
+        let assets = Assets::new(ctx)?;
+        let tetrimino = Tetrimino::from(TetriminoType::I);
+        let (width, height) = graphics::drawable_size(ctx);
+
+        let s = MainState {
+            assets,
+            screen_width: width,
+            screen_height: height,
+            tetrimino,
+            fall_timeout: FALL_TIME
+        };
+
+        Ok(s)
+    }
+}
+
+fn world_to_screen_coords(screen_width: f32, screen_height: f32, point: WorldPoint2) -> ScreenPoint2 {
+    let x = (point.x as f32) * (screen_width / 10.0);
+    let y = (point.y as f32) * (screen_height / 20.0);
+    ScreenPoint2::new(x, y)
+}
+
+fn draw_tetrimino(
+    assets: &mut Assets,
+    ctx: &mut Context,
+    tetrimino: &Tetrimino,
+    world_coords: (f32, f32),
+) -> GameResult {
+    let (screen_w, screen_h) = world_coords;
+    let pos = world_to_screen_coords(screen_w, screen_h, tetrimino.pos);
+    let image = assets.block_image(tetrimino);
+
+    for vector in tetrimino.vectors.iter() {
+        let vector_pos =
+            world_to_screen_coords(
+                screen_w,
+                screen_h,
+                WorldPoint2::from([vector.x + tetrimino.pos.x, vector.y + tetrimino.pos.y]));
+        let draw_params = graphics::DrawParam::new()
+            .dest(vector_pos);
+        graphics::draw(ctx, image, draw_params);
+    }
+
+    let draw_params = graphics::DrawParam::new()
+        .dest(pos);
+    graphics::draw(ctx, image, draw_params)
+}
+
+impl ggez::event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
-        self.dt = timer::delta(ctx);
+        const DESIRED_FPS: u32 = 60;
+
+        while timer::check_update_time(ctx, DESIRED_FPS) {
+            let seconds = 1.0 / (DESIRED_FPS as f32);
+
+            self.fall_timeout -= seconds;
+            if self.fall_timeout < 0.0 {
+                self.tetrimino.move_down();
+                self.fall_timeout = FALL_TIME;
+            }
+
+        }
         Ok(())
     }
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, BLACK);
-        let mut text = Text::default();
-        text.add(TextFragment::from(format!("Hello ggez! dt = {}ns", self.dt.subsec_nanos())));
-        graphics::draw(ctx, &text, DrawParam::default());
 
-        let square = Mesh::new_rectangle(
-            ctx,
-            DrawMode::fill(),
-            Rect::from([self.pos_x, 0.0, 50.0, 50.0]),
-            WHITE
-        )?;
+        {
+            let assets = &mut self.assets;
+            let coords = (self.screen_width, self.screen_height);
 
-        graphics::draw(ctx, &square, (na::Point2::new(100.0, 0.0),));
+            let tetrimino = &self.tetrimino;
+            draw_tetrimino(assets, ctx, tetrimino, coords)?;
+        }
 
         graphics::present(ctx)
     }
 
-    fn key_down_event(&mut self, ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods, _repeat: bool) {
-        let mut text = Text::default();
+    fn key_down_event(&mut self, _ctx: &mut Context, keycode: KeyCode, _keymods: KeyMods, _repeat: bool) {
         match keycode {
             KeyCode::Left => {
-                println!("Left key pressed");
-                text.add(TextFragment::from("Left key pressed"));
-                graphics::draw(ctx, &text, (na::Point2::new(200.0, 200.0),));
+                self.tetrimino.move_left();
             }
             KeyCode::Right => {
-                println!("Right key pressed");
-                text.add(TextFragment::from("Right key pressed"));
-                graphics::draw(ctx, &text, DrawParam::default());
-
-                self.pos_x = self.pos_x % 800.0 + 1.0;
+                self.tetrimino.move_right();
+            }
+            KeyCode::Down => {
+                self.tetrimino.move_down();
+            }
+            KeyCode::Up => {
+                self.tetrimino.move_up();
             }
             _ => ()
         }
     }
 }
 
-fn main() {
-    let state = &mut State {
-        dt: std::time::Duration::new(0, 0),
-        pos_x: 0.0
+fn main() -> GameResult {
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("resources");
+        path
+    } else {
+        path::PathBuf::from("./resources")
     };
 
     let mut config = conf::Conf::new();
     config.window_setup.title = String::from("Just another Tetris");
+    config.window_mode.width = 440.0;
+    config.window_mode.height = 880.0;
 
     let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("tetris", "Jose Matias Misiego Ruiz")
         .conf(config)
+        .add_resource_path(resource_dir)
         .build()
         .unwrap();
 
-    event::run(ctx, event_loop, state).unwrap();
+    let game = &mut MainState::new(ctx)?;
+
+    event::run(ctx, event_loop, game)
 }
